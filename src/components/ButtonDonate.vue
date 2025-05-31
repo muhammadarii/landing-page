e7827d30c61c31e10ce288902fa7aee543cf091c
ciyek <aripurnomo606@gmail.com>
2025-05-31 22:41:55 +0700

    initial commit

---
+++ src/components/ButtonDonate.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <button class="bg-amber-400 text-black">donate</button>
+</template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script></script>
+<template>
+  <div class="flex flex-col items-end">
+    <button class="border-3 border-black px-4 py-2 text-xl font-semibold m-4">
+      donate
+    </button>
+  </div>
+  <div class="flex flex-row justify-center items-center">
+    <h1 class="text-4xl font-bold">Hello world</h1>
+  </div>
+</template>
